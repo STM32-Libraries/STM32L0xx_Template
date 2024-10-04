cc144f33710a14610363d2f1a866515348fab083
MarcoAAG <marco-antonioa@outlook.com>
2024-10-03 23:00:16 -0600

    Update Detail Design

--- src/stm32l0xx_hal_msp.c
+++ src/stm32l0xx_hal_msp.c
@@ -1,110 +1,37 @@
-/**
- ******************************************************************************
- * @file    UART/UART_Printf/Src/stm32l0xx_hal_msp.c
- * @author  MCD Application Team
- * @brief   HAL MSP module.
- ******************************************************************************
- * @attention
+/**********************************************************************************************************************
+ * \file stm32l0xx_hal_msp.c
  *
+ * \author  STMicroelectronics
+ *
+ * \date Oct-03-2024
+ *
+ * \version 1.0 \n \n
+ *
+ *********************************************************************************************************************/
+
+/**********************************************************************************************************************
  * Copyright (c) 2016 STMicroelectronics.
  * All rights reserved.
  *
  * This software is licensed under terms that can be found in the LICENSE file
  * in the root directory of this software component.
  * If no LICENSE file comes with this software, it is provided AS-IS.
- *
- ******************************************************************************
- */
+ *********************************************************************************************************************/
 
-/* Includes ------------------------------------------------------------------*/
 #include "stm32l0xx_hal.h"
 
-/** @addtogroup STM32L0xx_HAL_Examples
- * @{
- */
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-/** @defgroup HAL_MSP
- * @brief HAL MSP module.
- * @{
- */
-
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
-/* Private function prototypes -----------------------------------------------*/
-/* Private functions ---------------------------------------------------------*/
-
-/** @defgroup HAL_MSP_Private_Functions
- * @{
- */
-
-/**
- * @brief UART MSP Initialization
- *        This function configures the hardware resources used in this example:
- *           - Peripheral's clock enable
- *           - Peripheral's GPIO Configuration
- * @param huart: UART handle pointer
- * @retval None
- */
 void HAL_UART_MspInit(UART_HandleTypeDef* huart)
 {
-  GPIO_InitTypeDef GPIO_InitStruct;
-
-  /*##-1- Enable peripherals and GPIO Clocks #################################*/
-  /* Enable GPIO TX/RX clock */
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-
-  /* Enable USARTx clock */
-  __HAL_RCC_USART2_CLK_ENABLE();
-
-  /*##-2- Configure peripheral GPIO ##########################################*/
-  /* UART TX GPIO pin configuration  */
-  GPIO_InitStruct.Pin       = GPIO_PIN_2;
-  GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull      = GPIO_PULLUP;
-  GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
-  GPIO_InitStruct.Alternate = GPIO_AF4_USART2;
-
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  /* UART RX GPIO pin configuration  */
-  GPIO_InitStruct.Pin       = GPIO_PIN_3;
-  GPIO_InitStruct.Alternate = GPIO_AF4_USART2;
-
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 }
 
-/**
- * @brief UART MSP De-Initialization
- *        This function frees the hardware resources used in this example:
- *          - Disable the Peripheral's clock
- *          - Revert GPIO and NVIC configuration to their default state
- * @param huart: UART handle pointer
- * @retval None
- */
 void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
 {
-  /*##-1- Reset peripherals ##################################################*/
-  __HAL_RCC_USART2_FORCE_RESET();
-  __HAL_RCC_USART2_RELEASE_RESET();
-
-  /*##-2- Disable peripherals and GPIO Clocks #################################*/
-  /* Configure UART Tx as alternate function  */
-  HAL_GPIO_DeInit(GPIOA, GPIO_PIN_2);
-  /* Configure UART Rx as alternate function  */
-  HAL_GPIO_DeInit(GPIOA, GPIO_PIN_3);
 }
 
-/**
- * @}
- */
-
-/**
- * @}
- */
-
-/**
- * @}
- */
+#ifdef __cplusplus
+}
+#endif
